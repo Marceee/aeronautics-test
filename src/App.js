629fa56ea47244e700ea1892ce828c38e0e66909
FMS <freelancemsn@gmail.com>
2017-11-20 17:11:23 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { GoogleMap, Marker } from "react-google-maps"
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="aeronautic">
+       <div className="aeronautic-title">
+        <bold>RunWay Info System</bold>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
